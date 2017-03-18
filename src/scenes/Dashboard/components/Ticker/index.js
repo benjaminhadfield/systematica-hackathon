c920a9b05f51fc7ac4e315e7e76e32ddcd097942
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-03-18 18:12:12 +0000

    styles

--- src/scenes/Dashboard/components/Ticker/index.js
+++ src/scenes/Dashboard/components/Ticker/index.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import styles from './styles.scss'
+const moment = require('moment')
 
 export const Ticker = ({marketData, ...props}) => {
     return (
         <div className={styles.container}>
             <h5 className={styles.title}>
-                <span className={styles.symbol}>APPL</span> Buy Prices
+                <span className={styles.symbol}>APPL</span> Bid Prices
             </h5>
             <ul className={styles.list}>
             {
-                marketData.map((item, i) => <li key={i} children={`${item.time}: $${item.bid}`}/>)
+                marketData.map((item, i) => {
+                    console.log(':)', item)
+                    const formattedDate = moment(item.time).format('HH:m ss:SSS')
+                    const price = '$' + (item.bid || '')
+                    return (
+                        <li key={i}>
+                            <span className={styles.time}>{formattedDate}</span>
+                            <span className={styles.price}>{price}</span>
+                        </li>
+                    )
+                })
             }
             </ul>
         </div>
