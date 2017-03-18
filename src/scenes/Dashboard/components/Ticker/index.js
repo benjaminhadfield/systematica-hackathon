89e5907c60077cf8c62e7bcf54cd5b853233dad8
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-03-18 17:57:17 +0000

    done

--- src/scenes/Dashboard/components/Ticker/index.js
+++ src/scenes/Dashboard/components/Ticker/index.js
@@ -4,9 +4,12 @@ import styles from './styles.scss'
 export const Ticker = ({marketData, ...props}) => {
     return (
         <div className={styles.container}>
-            <ul>
+            <h5 className={styles.title}>
+                <span className={styles.symbol}>APPL</span> Buy Prices
+            </h5>
+            <ul className={styles.list}>
             {
-                marketData.map((item, i) => <li key={i} children={item.bid}/>)
+                marketData.map((item, i) => <li key={i} children={`${item.time}: $${item.bid}`}/>)
             }
             </ul>
         </div>
